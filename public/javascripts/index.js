495402a290f7c308d4739b011b8c353f4632e737
James <jamesjaywoo@gmail.com>
2016-03-20 12:43:14 -0700

    updates

---
+++ public/javascripts/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function (){
+    $("#home-link").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 800);
+    });
+    $("#aboutme-link").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#aboutme").offset().top - 200
+        }, 800);
+    });
+    $("#resume-link").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#resume").offset().top - 200
+        }, 800);
+    });
+    $("#projects-link").click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top - 200
+        }, 800);
+    });
+});
